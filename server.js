6da64b1f16d9d8b350e3899ea816ae9a9cda3fb1
Tamara Hussien <tamarahussien97@gmail.com>
2020-11-05 22:59:11 +0200

    lab 12

--- server.js
+++ server.js
@@ -3,9 +3,11 @@
 let express = require('express');
 let app = express();
 let superagent = require('superagent');
+let pg = require('pg'); //postgresql
 require('dotenv').config();
 let PORT = process.env.PORT;
-
+let DATABASE_URL = process.env.DATABASE_URL;
+let client = new pg.Client(DATABASE_URL); //client is a middle between postgresql and server.js
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
@@ -14,7 +16,21 @@ app.set('view engine', 'ejs');
 
 //index page
 app.get('/', (request, response) => {
-    response.render('pages/index');
+    let DB = `SELECT * FROM books`;
+    client.query(DB).then((data)=>{
+        //to read the data
+        // console.log(data); 
+        // response.send(data); 
+        //variable to save the rows from the data object
+        let DBrow = data.rows;
+        // response.send(DBrow); //test
+        
+        response.render('pages/index',{x:DBrow,
+        y:data.rowCount});
+    })
+    .catch(error =>{
+        console.log('error');
+    })
 });
 
 //search form page
@@ -47,10 +63,17 @@ function Book(bookObj) {
     this.author = bookObj.volumeInfo.authors ?bookObj.volumeInfo.authors :'No title authors' ;
     this.description = bookObj.volumeInfo.description?bookObj.volumeInfo.description:'No title description' ;
     this.image = bookObj.volumeInfo.imageLinks? bookObj.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
-
 }
 
 //port
-app.listen(PORT, () => {
-    console.log(`Listening to port ... ${PORT}`);
+client.connect().then( () => {
+    app.listen(PORT, () => {
+
+        console.log(`Listening to port ... ${PORT}`);
+    
+    });
+
+}).catch(error =>{
+    console.log('error connect to DB' , error);
 });
+
