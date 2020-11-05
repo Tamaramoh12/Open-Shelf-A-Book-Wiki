aea2e198a4c34a00f908fa4eac37e8f1a98d4bb2
Tamara Hussien <tamarahussien97@gmail.com>
2020-11-06 01:24:18 +0200

    lab12

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ let express = require('express');
 let app = express();
 let superagent = require('superagent');
 let pg = require('pg'); //postgresql
+let methodOverride = require('method-override');
+
 require('dotenv').config();
 let PORT = process.env.PORT;
 let DATABASE_URL = process.env.DATABASE_URL;
@@ -14,9 +16,13 @@ app.use(express.static('./public'));
 // app.use('/public/styles',express.static(__dirname+'public/styles'));
 app.set('view engine', 'ejs');
 
-//index page
-app.get('/', (request, response) => {
-    let DB = `SELECT * FROM books`;
+app.use(methodOverride('_method'));
+
+//index
+app.get('/',homepage);
+
+function homepage(request,response){
+    let DB = `SELECT * FROM books;`;
     client.query(DB).then((data)=>{
         //to read the data
         // console.log(data); 
@@ -31,13 +37,38 @@ app.get('/', (request, response) => {
     .catch(error =>{
         console.log('error');
     })
-});
+}
+//
+app.get('/books/:id',handleBooks);
+
+function handleBooks(request,response){
+    let URLid = request.params.id;
+    let DB = `SELECT * FROM books WHERE id = ${URLid};`;
+    client.query(DB).then((data) => {
+        response.render('pages/books/show',{x:data.rows[0]});
+    });
+}
 
 //search form page
 app.get('/searches/new', (request, response) => {
     response.render('searches/new');
 });
+
 //
+app.put ('/books/:id', bookUpdate);
+
+function bookUpdate(req,res){
+    let recievedUpdate = req.body;
+    let statement = `UPDATE books SET title =$1, Author=$2, isbn=$3, image_url=$4, descr=$5  WHERE id=$6;`;
+    let values = [recievedUpdate.title, recievedUpdate.author, recievedUpdate.isbn, recievedUpdate.image_url, recievedUpdate.descr, recievedUpdate.id];
+    client.query(statement, values).then( data =>{
+      res.redirect(`/books/${recievedUpdate.id}`);
+      console.log('item updated ' + recievedUpdate.id);
+    }).catch((error) => {
+      console.log('error happend in the updated data...',error);
+    });
+  }
+  
 app.post('/searches', (request, response) => {
     let result = request.body;
 
@@ -56,6 +87,22 @@ app.post('/searches', (request, response) => {
 
     // response.send(result); //test
 });
+
+//
+app.post ('/books', HandellBooks);
+
+function HandellBooks(req, res){
+  let newBookAdded = req.body;
+  let statement = `INSERT INTO books (title, Author, isbn, image_url, descr) VALUES ($1,$2,$3,$4,$5) RETURNING id ;`;
+  let values = [newBookAdded.title,newBookAdded.author,newBookAdded.isbn,newBookAdded.image_url,newBookAdded.descr];
+  client.query(statement,values).then( data =>{
+    console.log(data.rows[0].id,'insid the book/is');
+    res.redirect(`/books/${data.rows[0].id}`);
+
+  }).catch((error) => {
+    console.log('error happend in the HandellBookID SQL',error);
+  });
+}
 
 //
 function Book(bookObj) {
