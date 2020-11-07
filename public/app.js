0db1bc421bc73419473960b8d2df5392930ca178
Tamara Hussien <tamarahussien97@gmail.com>
2020-11-07 07:48:11 +0200

    finish all the functionality

---
+++ public/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$(document).ready(function(){
+    $('#updatebutton').click(function () {
+      console.log('clicked');
+      $(this).hide();
+      $('#deleteFromMain').toggle();
+    });
+  
+  });
+  
